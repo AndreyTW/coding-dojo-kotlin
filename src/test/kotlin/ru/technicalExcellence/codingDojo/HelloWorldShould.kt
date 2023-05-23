b8c0c597778e4c624186ae40b0616d07b3ae0480
Andrey_TW <sav_687@mail.ru>
2023-05-23 23:26:41 +0300

    7 kyu - Geometric Progression Sequence

--- src/test/kotlin/ru/technicalExcellence/codingDojo/HelloWorldShould.kt
+++ src/test/kotlin/ru/technicalExcellence/codingDojo/HelloWorldShould.kt
@@ -2,16 +2,29 @@ package ru.technicalExcellence.codingDojo
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.math.pow
 
 class HelloWorldShould {
 
+//    @Test
+//    fun returnHelloWorld() {
+//        val expected = "Hello World!"
+//
+//        val actual = HelloWorld().helloWorld();
+//
+//        assertEquals(expected, actual)
+//    }
+
+    private fun test(actual: String, expected: String) = assertEquals(expected, actual)
+
     @Test
-    fun returnHelloWorld() {
-        val expected = "Hello World!"
-
-        val actual = HelloWorld().helloWorld();
-
-        assertEquals(expected, actual)
+    fun exampleTests() {
+        test(geometricSequenceElements(2, 3, 5), "2, 6, 18, 54, 162")
+        test(geometricSequenceElements(2, 2, 10), "2, 4, 8, 16, 32, 64, 128, 256, 512, 1024")
+        test(geometricSequenceElements(1, -2, 10), "1, -2, 4, -8, 16, -32, 64, -128, 256, -512")
     }
+
+    fun geometricSequenceElements(a: Int, r: Int, n: Int): String =
+        LongArray(n) { i -> a * r.toDouble().pow(i.toDouble()).toLong() }.joinToString(", ")
 }
 
