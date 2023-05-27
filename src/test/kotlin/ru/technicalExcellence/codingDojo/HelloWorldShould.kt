6984ce1c1cc6a92b5f97c1aaefb9469e76186e14
Andrey_TW <sav_687@mail.ru>
2023-05-27 23:57:45 +0300

    7 kyu - ATM

--- src/test/kotlin/ru/technicalExcellence/codingDojo/HelloWorldShould.kt
+++ src/test/kotlin/ru/technicalExcellence/codingDojo/HelloWorldShould.kt
@@ -54,16 +54,16 @@ class HelloWorldShould {
 //            .take(n)
 //            .joinToString(", ")
 
-    private fun testing(actual:Int, expected:Int) {
-        assertEquals(expected, actual)
-    }
-    @Test
-    fun test1() {
-        println("Fixed Tests: movie")
-        testing(movie(500, 15, 0.9), 43)
-        testing(movie(100, 10, 0.95), 24)
-
-    }
+//    private fun testing(actual:Int, expected:Int) {
+//        assertEquals(expected, actual)
+//    }
+//    @Test
+//    fun test1() {
+//        println("Fixed Tests: movie")
+//        testing(movie(500, 15, 0.9), 43)
+//        testing(movie(100, 10, 0.95), 24)
+//
+//    }
 
 //    fun movie(card:Int, ticket:Int, perc:Double):Int {
 //        var n = 0
@@ -91,10 +91,54 @@ class HelloWorldShould {
 //        return n
 //    }
 
-    fun movie(card:Int, ticket:Int, perc:Double) = generateSequence(1){it + 1}.
-    first {
-        ticket * it > ceil(card + ticket * perc * (1 - perc.pow(it)) / (1 - perc))
+//    fun movie(card:Int, ticket:Int, perc:Double) = generateSequence(1){it + 1}.
+//    first {
+//        ticket * it > ceil(card + ticket * perc * (1 - perc.pow(it)) / (1 - perc))
+//    }
+
+    @Test
+    fun `ATM Payment Possible`() {
+        assertEquals(4, count(770))
+        assertEquals(1, count(10))
     }
 
-}
+    @Test
+    fun `ATM Payment Impossible`() {
+        assertEquals(-1, count(125))
+        assertEquals(-1, count(238))
+    }
 
+//    fun count(number: Int): Int {
+//        val n = when {
+//            number % 10 == 0 -> number / 500 +
+//                                number % 500 / 200 +
+//                                number % 500 % 200 / 100 +
+//                                number % 500 % 200 % 100 / 50 +
+//                                number % 500 % 200 % 100 % 50 / 20 +
+//                                number % 500 % 200 % 100 % 50 % 20 / 10
+//            else -> -1
+//        }
+//        return n
+//    }
+
+//    fun count(number: Int): Int = when {
+//        number % 10 == 0 -> number / 500 +
+//                number % 500 / 200 +
+//                number % 500 % 200 / 100 +
+//                number % 500 % 200 % 100 / 50 +
+//                number % 500 % 200 % 100 % 50 / 20 +
+//                number % 500 % 200 % 100 % 50 % 20 / 10
+//
+//        else -> -1
+//    }
+
+    fun count(number: Int) = if (number % 10 > 0) -1 else listOf(500, 200, 100, 50, 20, 10)
+        .fold(listOf(number, 0)) { acc, i -> listOf(acc[0] % i, acc[1] + acc[0] / i) }[1]
+
+//    val values = listOf(500, 200, 100, 50, 20, 10)
+//
+//    fun count(amount: Int): Int {
+//        val (a, k) = values.fold(Pair(amount, 0)){ (a, k), v -> Pair(a % v, k + a / v) }
+//        return if (a == 0) k else -1
+//    }
+}
